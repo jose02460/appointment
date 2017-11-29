8184a58980ade8f640d966562a2fee05d323a765
Jose Rodriguez and Ryan Ferris <jose@>
2017-11-29 15:57:34 -0800

    add style rules to the styles.css file and script rules to scripts.js file

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  $("form#scheduleAnAppointment").submit(function(event) {
+    event.preventDefault();
+
+    var firstNameOutput = $("#firstName").val();
+    var lastNameOutput = $("#lastName").val();
+    var descriptionOutput = $("#discription").val();
+    var startDateOutput = $("#startDate").val();
+    var endDateOutput = $("#endDate").val();
+
+    $("#firstNameOutput").text(firstNameOutput);
+    $("#lastNameOutput").text(lastNameOutput);
+    $("#descriptionOutput").text(descriptionOutput);
+    $("#startDateOutput").text(startDateOutput);
+    $("#endDateOutput").text(endDateOutput);
+
+    $("#confirmation").show();
+    $("form#scheduleAnAppointment").hide();
+  });
+
+
+
+
+})
